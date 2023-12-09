0e43d0d17fd87ca3dbddc9f3d8831395c0d3780e
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-10 04:20:54 +0600

    bug fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const { user } = ((await getUserServerSession()) as Session) || null;
+  const { user } = ((await getUserServerSession()) as Session) || {};
 
   return (
     <html lang="en" suppressHydrationWarning>
