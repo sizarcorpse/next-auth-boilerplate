c13ae398dfdb712aaa3be161115c5a2b8c30819d
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-16 03:43:14 +0600

    package,index

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,2 +1,7 @@
 export { default as getCurrentUser } from "./getCurrentUser";
 export { default as getUserServerSession } from "./getUserServerSession";
+export { default as getUserToken } from "./getUserToken";
+export { default as getUser } from "./user/getUser";
+export { default as getUserProfile } from "./user/getUserProfile";
+export { default as updateUser } from "./user/updateUser";
+export { default as updateUserProfile } from "./user/updateUserProfile";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,49 +14,30 @@ const about = {
   ],
 };
 
-import { getCurrentUser, getUserServerSession } from "@/actions";
-import { authOptions } from "@/libs/auth";
-import { getServerSession } from "next-auth/next";
+import Link from "next/link";
 
 const Home = async () => {
   const { packages } = about;
-  const user = await getCurrentUser();
-  const session = await getServerSession(authOptions);
-  const userSessionServer = await getUserServerSession();
 
   return (
     <main className="flex items-center justify-center p-6">
       <div className="container max-w-screen-lg mx-auto">
+        <Link href="/u">User</Link>
+        <Link href="/u/settings">Settings</Link>
+
         <div className="flex flex-col gap-4 items-center justify-center">
-          <h1 className="text-xl font-bold text-slate-800 uppercase md:text-4xl lg:text-6xl text-center">
+          <h1 className="text-xl font-bold text-primary uppercase md:text-4xl lg:text-6xl text-center">
             Next Auth Boilerplate
           </h1>
           <div className="flex flex-row items-center justify-center flex-wrap gap-2">
             {packages.map((item, index) => (
               <p
                 key={index}
-                className="px-2 py-1 rounded-lg border border-slate-300 text-sm text-slate-800 font-semibold cursor-pointer hover:bg-amber-300 hover:border-rose-400"
+                className="px-2 py-1 rounded-lg border border-slate-300 text-sm text-primary font-semibold cursor-pointer hover:bg-amber-300 hover:border-rose-400"
               >
                 {item}
               </p>
             ))}
-          </div>
-          <div className="max-w-screen-xl">
-            <pre>
-              <code>
-                getCurrentUser:
-                {JSON.stringify(user, null, 2)}
-              </code>
-            </pre>
-            <pre>
-              <code>session:{JSON.stringify(session, null, 2)}</code>
-            </pre>
-            <pre>
-              <code>
-                getUserServerSession:
-                {JSON.stringify(userSessionServer, null, 2)}
-              </code>
-            </pre>
           </div>
         </div>
       </div>
