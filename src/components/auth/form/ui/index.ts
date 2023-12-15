1e372cd92afce322df61f474b7ecc15718fb5e82
Sizar Corpse <sizarcorpse@gmail.com>
2023-12-16 03:48:23 +0600

    Remove old files and dirs

--- src/components/auth/form/ui/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as FormControl } from "./FormControl";
-export { default as FormDataFetchError } from "./FormDataFetchError";
-export { default as FormGroup } from "./FormGroup";
--- src/components/auth/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as SignInForm } from "./form/SignInForm";
-export { default as SignUpForm } from "./form/SignUpForm";
