3e359113c000f7d60f44bbeb638b7f185e260c38
Sizar Corpse <sizarcorpse@gmail.com>
2024-01-01 22:08:39 +0600

    Update next.config.js and package.json

--- next.config.js
+++ next.config.js
@@ -4,16 +4,54 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-const nextConfig = {
-  env: {
-    GITHUB_ID: process.env.GITHUB_ID,
-    GITHUB_SECRET: process.env.GITHUB_SECRET,
-    NEXT_API_URL: process.env.NEXT_API_URL || "http://localhost:3000/api",
-  },
-  images: {
-    domains: ["avatars.githubusercontent.com", "images.unsplash.com"],
-  },
-  transpilePackages: ["lucide-react"],
-};
+let nextConfig;
+
+if (process.env.NODE_ENV === "production") {
+  nextConfig = {
+    env: {
+      DATABASE_URL: process.env.DATABASE_URL_PROD,
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET_PROD,
+      GITHUB_ID: process.env.GITHUB_ID_PROD,
+      GITHUB_SECRET: process.env.GITHUB_SECRET_PROD,
+      NEXT_API_URL: process.env.NEXT_API_URL_PROD,
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL_PROD,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "images.unsplash.com",
+        },
+        {
+          protocol: "https",
+          hostname: "avatars.githubusercontent.com",
+        },
+      ],
+    },
+  };
+} else {
+  nextConfig = {
+    env: {
+      DATABASE_URL: process.env.DATABASE_URL,
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+      GITHUB_ID: process.env.GITHUB_ID,
+      GITHUB_SECRET: process.env.GITHUB_SECRET,
+      NEXT_API_URL: process.env.NEXT_API_URL,
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "images.unsplash.com",
+        },
+        {
+          protocol: "https",
+          hostname: "avatars.githubusercontent.com",
+        },
+      ],
+    },
+  };
+}
 
 module.exports = withBundleAnalyzer(nextConfig);
