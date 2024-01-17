b35cd35f4574395457664425eabf4eb56603d555
Sizar Corpse <sizarcorpse@gmail.com>
2024-01-18 03:17:46 +0600

    Update user settings layout and add code snippets

--- src/app/(pages)/(user)/u/profile/settings/layout.tsx
+++ src/app/(pages)/(user)/u/profile/settings/layout.tsx
@@ -9,7 +9,7 @@ export default function UserSettingsLayout({
   profile: React.ReactNode;
 }) {
   return (
-    <main className="container max-w-screen-md">
+    <main className="container max-w-screen-md px-2">
       <Tabs defaultValue="profile" orientation="vertical">
         <TabsList className="bg-primary/5 p-1 h-auto mb-6">
           <TabsTrigger value="profile" className="h-10 px-10">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const about = {
   packages: [
     "NextJS",
@@ -7,10 +9,11 @@ const about = {
     "Prisma",
     "PostgreSQL",
     "React Hook Form",
-    "React-hot-toast",
+    "React Hot Toast",
     "Swr",
-    "React-icons",
     "Zod",
+    "Shadcn UI",
+    "Lucide React",
   ],
 };
 
@@ -18,10 +21,10 @@ const Home = async () => {
   const { packages } = about;
 
   return (
-    <main className="flex items-center justify-center p-6">
+    <main className="flex items-center justify-center">
       <div className="container max-w-screen-lg mx-auto">
-        <div className="flex flex-col gap-4 items-center justify-center">
-          <h1 className="text-xl font-bold text-primary uppercase md:text-4xl lg:text-6xl text-center">
+        <div className="flex flex-col gap-4 items-center justify-center h-[calc(100vh-52px)]">
+          <h1 className="text-xl font-black text-primary uppercase tracking-wider md:text-4xl lg:text-5xl text-center">
             Next Auth Boilerplate
           </h1>
 
@@ -29,11 +32,29 @@ const Home = async () => {
             {packages.map((item, index) => (
               <p
                 key={index}
-                className="px-2 py-1 rounded-lg border border-slate-300 text-sm text-primary font-semibold cursor-pointer hover:bg-amber-300 hover:border-rose-400"
+                className="px-2 py-2 rounded-lg border border-border text-sm text-primary font-semibold cursor-pointer md:px-4 md:py-4 "
               >
                 {item}
               </p>
             ))}
+          </div>
+
+          <div className="bg-primary/5 p-2 rounded-lg md:p-4">
+            <pre className="flex flex-col gap-1 whitespace-break-spaces">
+              <code className="text-sm text-primary">
+                {"$"}{" "}
+                {`git clone https://github.com/sizarcorpse/next-auth-boilerplate.git`}
+              </code>
+              <code className="text-sm text-primary">
+                {"$"} {`cd next-auth-boilerplate`}
+              </code>
+              <code className="text-sm text-primary">
+                {"$"} {`npm install`}
+              </code>
+              <code className="text-sm text-primary">
+                {"$"} {`npm run dev`}
+              </code>
+            </pre>
           </div>
         </div>
       </div>
